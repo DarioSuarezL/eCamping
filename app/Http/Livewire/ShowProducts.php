e4a658e6bf384884af00198b18c1e873123bd0a3
DarioS <dsuarezlazarte@gmail.com>
2023-05-07 18:28:52 -0400

    eight commit - 257

--- app/Http/Livewire/ShowProducts.php
+++ app/Http/Livewire/ShowProducts.php
@@ -3,12 +3,23 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class ShowProducts extends Component
 {
-    protected $listeners = ['deleteProduct'];
+    public $term;
+    public $category;
+    public $brand;
+
+    protected $listeners = ['deleteProduct', 'findProduct'];
+
+    public function findProduct($term, $category, $brand){
+        $this->term = $term;
+        $this->category = $category;
+        $this->brand = $brand;
+    }
 
     public function deleteProduct(Product $product){
         Storage::delete('public/products/'.$product->imagen);
@@ -17,7 +28,18 @@ class ShowProducts extends Component
     
     public function render()
     {
-        $products = Product::paginate(18);
+        // $products = Product::paginate(18);
+        $products = Product::when($this->term, function($query){
+            $query->where(DB::raw('LOWER(nombre)'), 'LIKE', "%".strtolower($this->term)."%");
+        })
+        ->when($this->category, function($query){
+            $query->where('category_id', $this->category);
+        })
+        ->when($this->brand, function($query){
+            $query->where('brand_id', $this->brand);
+        })
+        ->paginate(18);
+
         return view('livewire.show-products',[
             'products' => $products
         ]);
