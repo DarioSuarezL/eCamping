995cb1fa19e9883a47b342e8185078f514459bd9
DarioS <dsuarezlazarte@gmail.com>
2023-05-09 12:19:13 -0400

    final commit

--- resources/views/livewire/show-methods.blade.php
+++ resources/views/livewire/show-methods.blade.php
@@ -1,6 +1,6 @@
 <div>
-    <form wire:submit.prevent="buy" class="">
-        <div class="p-5 bg-gray-800 rounded-lg mt-8 w-1/3">
+    <form wire:submit.prevent="buy" class="p-3">
+        <div class="p-2 bg-gray-800 rounded-lg mt-8 w-auto">
             <x-input-label for="method" :value="__('Método de pago')" />
             <select wire:model='payment_method_id'>
                 <option value="" hidden> --Seleccione metodo-- </option>
@@ -10,7 +10,7 @@
             </select>
         </div>
         <button type="submit" class="text-white bg-red-800 hover:bg-red-700 p-2 rounded-lg mt-8 h-1/2">
-            {{ __('Seleccionar') }}
+            {{ __('Pagar') }}
         </button>
     </form>
 </div>
